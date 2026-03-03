a558350ab4e11fdf2f2c55b3320ada14dcfae174
Reece Wanvig <rwanvig@emeris.ac.za>
2026-03-03 10:55:23 +0200

    finished activity for the spec

--- app/src/main/java/com/example/prjfragments2026/MenuActivity.kt
+++ app/src/main/java/com/example/prjfragments2026/MenuActivity.kt
@@ -5,7 +5,12 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
+private val Frag1 = FirstFragment()
+private val Frag2 = SecondFragment()
+private val Frag3 = DuckFragment()
 class MenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,7 +21,26 @@ class MenuActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        replaceFrag(Frag3)
+        val bottomBar =findViewById<BottomNavigationView>(R.id.NavBar)
+        bottomBar.setOnItemSelectedListener {
+            when (it.itemId)
+            {
+                R.id.ic_home->replaceFrag(Frag3)
+                R.id.ic_setting->replaceFrag(Frag2)
+                R.id.ic_duck->replaceFrag(Frag1)
+            }
+            true
+        }
 
-
+    }
+    private fun replaceFrag(fragment: Fragment)
+    {
+        if(fragment!=null)
+        {
+            val transaction = supportFragmentManager.beginTransaction()
+            transaction.replace(R.id.frameLayout,fragment)
+            transaction.commit()
+        }
     }
 }
